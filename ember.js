59200f4ca9685e36a05f7def0228a1d3dcabd1b0
Sebi <sebigyger@pm.me>
2025-03-14 23:38:41 -0400

    Add files via upload

--- ember.js
+++ ember.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/47604112
 const checkVideoData = async () => {
-    let videoData = await fetch(`./examplevidchat.json`)
+    let videoData = await fetch(`./chat.json`)
         .then((response) => { 
             return response.json().then((data) => {
                 console.log(data);
@@ -47,6 +47,65 @@ function timeSince(date) {
     return Math.floor(seconds) + " seconds";
 }
 
+// Code modified from https://github.com/JZimz/tmi-utils to fit the data gotten from TwitchDownloader for the next two functions
+
+function getEmoteAsUrl (id, theme = 'light', scale = '1.0') {
+    return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/${theme}/${scale}`;
+}
+
+// Split the message into emotes and text by using the emote positions
+function parseEmotesInMessage (emotes, msg) {
+    const result = [];
+    // Don't bother with the rest if there are no emotes
+    if (emotes.length == 0) {
+        result.push({
+            type: 'text', 
+            value: msg
+        })
+        return result;
+    }
+  
+    // Split the message in a unicode-safe way
+    const msgArray = Array.from(msg)
+    const emotePositions = [];
+
+    for (let i = 0; i < emotes.length; i++) {
+        let id = emotes[i]._id;
+        let begin = emotes[i].begin;
+        let end = emotes[i].end;
+        emotePositions.push({id: id, begin: begin, end: end});
+    }
+
+    let cursor = 0;
+
+    for (let i = 0; i < emotePositions.length; i++) {
+      // Add any text before the first emote.
+      if (emotePositions[i].begin > cursor) {
+        result.push({
+          type: 'text',
+          value: msgArray.slice(cursor, emotePositions[i].begin).join('')
+        })
+      }
+  
+      result.push({
+        type: 'emote',
+        raw: msgArray.slice(emotePositions[i].begin, emotePositions[i].end + 1).join(''),
+        value: `${emotePositions[i].id}`
+      })
+      cursor = emotePositions[i].end + 1
+    }
+  
+    // Add any remaining text after the last emote.
+    if (cursor < msgArray.length) {
+      result.push({
+        type: 'text',
+        value: msgArray.slice(cursor).join('')
+      })
+    }
+
+    return result;
+};
+
 // Compare time offsets to decide when to post a comment
 const getVideoTime = async () => {
     let jsonData = await checkVideoData();
@@ -60,13 +119,36 @@ const getVideoTime = async () => {
             let commentCreationOffset = jsonData.comments[i].content_offset_seconds;
 
             if (commentCreationOffset <= videoTime) {
-                let timestamp = document.createElement('span');
-                let username = document.createElement('span');
-                let message = document.createElement('span');
+                // add a semicolon before the message 
+                const parsedMessage = parseEmotesInMessage(jsonData.comments[i].message.emoticons, jsonData.comments[i].message.body);
+                const timestamp = document.createElement('span');
+                const username = document.createElement('span');
+                const message = document.createElement('span');
+
+                message.append(': ');
+                for (let i = 0; i < parsedMessage.length; i++) {
+                    console.log(parsedMessage[i].value);
+                //parsedMessage.forEach(({ type, value, raw }) => {
+                    if (parsedMessage[i].type === 'emote') {
+                      const img = new Image();
+                      // Converts the emote id to URL
+                      img.src = getEmoteAsUrl(parsedMessage[i].value);
+                      // Raw is the original emote text (e.g. LUL)
+                      img.alt = parsedMessage[i].raw;
+                      img.title = parsedMessage[i].raw;
+                
+                      message.append(img);
+                    } else {
+                      message.append(parsedMessage[i].value);
+                    }
+                }
+
                 timestamp.innerHTML = '[' + jsonData.comments[i].created_at.slice(11, 19) + '] ';
                 username.innerHTML = jsonData.comments[i].commenter.display_name;
                 username.style.color = jsonData.comments[i].message.user_color;
-                message.innerHTML = ': ' + jsonData.comments[i].message.body;
+
+
+                //message.innerHTML = ': ' + jsonData.comments[i].message.body;
                 document.getElementById('chatContainer').appendChild(timestamp);
                 document.getElementById('chatContainer').appendChild(username);
                 document.getElementById('chatContainer').appendChild(message);
