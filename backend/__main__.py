e863f6f45be542e18036cb61ff5453b8cdfafea5
Wilson Van <wilsonvan003@gmail.com>
2025-04-09 15:40:41 -0400

    Made arg parser a method

--- backend/__main__.py
+++ backend/__main__.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 
 
 if __name__ == '__main__':
+    # create the parser
+    parser = cli.create_parser()
+    # read arguments from the cli
+    arguments = parser.parse_args()
+    # parse arguments into variables
+    directory = arguments.directory.replace(" ", "_")
+    host = arguments.bind
+    port = arguments.PORT
     print('Visit http://localhost:8000/index.html for the player demo')
-    scanner.get_file_list("./temp_video")
+    scanner.get_file_list(directory)
     app = create_app()
     app.run(host='0.0.0.0', port=8000, debug=False)
--- backend/cli.py
+++ backend/cli.py
@@ -1,46 +1,38 @@
 import argparse
-from pathlib import Path
 
-# creates a parser which will parse commands entered through the CLI
-parser = argparse.ArgumentParser(description = 'Parser that will parse the command arguments for a CLI')
 
-# a list of arguments passed through the cli
+def create_parser():
+    """Create a argument parser that will take arguments passed through the CLI"""
+    # creates a parser which will parse commands entered through the CLI
+    parser = argparse.ArgumentParser(description = 'Parser that will parse the command arguments for a CLI')
 
-# binding flag
-# a flag which will bind the web server to the str argument (address) that is given
-parser.add_argument(
-    '-b', '--bind', 
-    help='binds the web server to the given address which by default is "0.0.0.0"',
-    type=str,
-    default='0.0.0.0'
-)
+    # a list of arguments passed through the cli
 
-# port flag
-# a flag that will bind the web server to the given int argument (port)
-parser.add_argument(
-    '-P', '--PORT',
-    help='binds the web server to a given port which by default is 80',
-    type=int,
-    default=80
-)
+    # binding flag
+    # a flag which will bind the web server to the str argument (address) that is given
+    parser.add_argument(
+        '-b', '--bind', 
+        help='binds the web server to the given address which by default is "0.0.0.0"',
+        type=str,
+        default='0.0.0.0'
+    )
 
-# directory flag
-# a required flag which will specify to Ember Archive which directory to scan for video and chat files
-parser.add_argument(
-    '-d', '--directory',
-    help='specifies the directory for Ember Archive to scan and monitor for video and chat files',
-    type=str,
-    required=True
-)
+    # port flag
+    # a flag that will bind the web server to the given int argument (port)
+    parser.add_argument(
+        '-P', '--PORT',
+        help='binds the web server to a given port which by default is 80',
+        type=int,
+        default=80
+    )
 
-# parses the arguments
-arguments = parser.parse_args()
+    # directory flag
+    # a required flag which will specify to Ember Archive which directory to scan for video and chat files
+    parser.add_argument(
+        '-d', '--directory',
+        help='specifies the directory for Ember Archive to scan and monitor for video and chat files',
+        type=str,
+        required=True
+    )
 
-# stores the directory path into  a path variable
-path = Path(arguments.directory)
-
-# a set of all the files we actually care about
-file_extensions = {".mp4", ".mkv", ".avi", ".mov", ".flv", ".wmv", ".webm", "json"}
-
-# list comprension to sort the files in the directory to only be the accepted file extensions
-ea_files = [file for file in path.rglob("*") if file.suffix.lower() in file_extensions]
+    return parser
