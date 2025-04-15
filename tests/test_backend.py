988b44e358cf509fd5b76399c6515d05292ee3b0
Wilson Van <wilsonvan003@gmail.com>
2025-04-15 14:25:51 -0400

    CLI test cases
    added test cases for the cli parser

---
+++ tests/test_backend.py
@@ -0,0 +1,45 @@
+import pytest
+from backend import Video, Channel, cli
+
+# tests for the cli
+
+def test_given_all_args():
+    parser = cli.create_parser()
+    parser.parse_args(['-b', "192.0.2.5", '-P', 8000, '-d', './test_videos'])
+    # the following are the expected values
+    expected_bind = "192.0.2.5"
+    expected_port = 8000
+    expected_directory = "./test_videos"
+    
+    assert parser.bind == expected_bind
+    assert parser.PORT == expected_port
+    assert parser.directory == expected_directory
+
+def test_directory_is_required():
+    parser = cli.create_parser()
+    
+    with pytest.raises(SystemError):
+        parser.parse_args([])
+
+def test_default_bind_and_port():
+    parser = cli.create_parser()
+    parser.parse_args(['-d', './test_videos'])
+
+    expected_bind = '0.0.0.0'
+    expected_port = 80
+
+    assert parser.bind == expected_bind
+    assert parser.PORT == expected_port
+
+def test_error_given_wrong_type():
+    # port is expected to be an int
+    parser = cli.create_parser()
+    
+    # give the parser a string instead of int port should raise an error
+    with pytest.raises(SystemError):
+        parser.parse_args(['-b', "192.0.2.5", '-P', 'hi', '-d', './test_videos'])
+
+
+# tests for the Video Class
+
+# tests for the Channel Class
